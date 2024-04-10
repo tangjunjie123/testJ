a27b7a14c42bc163ed7b90c81181234b2281e0dd
tangjunjie123 <88720204+tangjunjie123@users.noreply.github.com>
2024-04-10 15:17:19 +0800

    Add files via upload

---
+++ jenkins/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	engine := gin.Default()
+	engine.GET("/", hello)
+	engine.Run(":7777")
+}
+
+// Handler
+func hello(c *gin.Context) {
+	c.JSON(http.StatusOK, "Hello, World!")
+}
